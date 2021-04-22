192faeeea1fe132f2b807519dfe3f5abd81f755c
Pad-Todd <pad.todd@gmail.com>
2021-04-22 17:42:19 +0900

    Nice!!

--- app/src/main/java/com/spaceplanning/android/space_planning_version_2/MainActivity.java
+++ app/src/main/java/com/spaceplanning/android/space_planning_version_2/MainActivity.java
@@ -1,14 +1,127 @@
 package com.spaceplanning.android.space_planning_version_2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity{
+
+    private static final int RC_SIGN_IN = 1000;
+    private static List<AuthUI.IdpConfig> providers = null;
+    // Bottom-Bar
+    private BottomNavigationView mBottomNV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signin();
+
+        //Bottom-Bar Setting
+        mBottomNV = findViewById(R.id.nav_view);
+        mBottomNV.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                BottomNavigate(item.getItemId());
+                return true;
+            }
+        });
+        mBottomNV.setSelectedItemId(R.id.navigation_1);
     }
+
+    private void BottomNavigate(int itemId) {
+        String tag = String.valueOf(itemId);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment currentFragment = fragmentManager.getPrimaryNavigationFragment();
+
+        if(currentFragment != null) {
+            fragmentTransaction.hide(currentFragment);
+        }
+
+        Fragment fragment = fragmentManager.findFragmentByTag(tag);
+        if (fragment == null) {
+            if (itemId == R.id.navigation_1) {
+                fragment = new FragmentPage1();
+
+            } else if (itemId == R.id.navigation_2){
+                fragment = new FragmentPage2();
+            }else {
+                fragment = new FragmentPage3();
+            }
+            fragmentTransaction.add(R.id.content_layout, fragment, tag);
+        } else {
+            fragmentTransaction.show(fragment);
+        }
+
+        fragmentTransaction.setPrimaryNavigationFragment(fragment);
+        fragmentTransaction.setReorderingAllowed(true);
+        fragmentTransaction.commitNow();
+    }
+
+    private void signin() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(getSelectedTheme())
+                        .setLogo(getSelectedLogo())
+                        .setAvailableProviders(getProviders())
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    private List<AuthUI.IdpConfig> getProviders() {
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+        return providers;
+    }
+
+    private int getSelectedLogo() {
+        return AuthUI.NO_LOGO;
+    }
+
+    private int getSelectedTheme() {
+        return AuthUI.getDefaultTheme();
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+
 }
